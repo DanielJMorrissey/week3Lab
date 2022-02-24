fd7f70f4937f9c56967eb012798ee8b9db2a5950
DanielJMorrissey <danieljohnmorrissey@hotmail.com>
2022-02-24 20:12:14 +0000

    added continuous delivery

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+sudo apt update && sudo apt install nodejs npm
+
+sudo npm install -g pm2
+
+pm2 stop example_app
+
+cd week3Lab
+
+npm install
+
+pm2 start ./bin/www --name example_app
