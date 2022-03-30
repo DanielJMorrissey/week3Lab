7b124720500475d685ddf2c6719bd2acc36f5295
DanielJMorrissey <danieljohnmorrissey@hotmail.com>
2022-03-30 14:35:51 +0100

    added Docker

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/env bash
-sudo apt update && sudo apt install nodejs npm
 
-sudo npm install -g pm2
+CURRENT_INSTANCE=$(docker ps -a -q --filter ancestor="$IMAGE_NAME" --format="{{.ID}}")
 
-pm2 stop example_app
 
-cd week3Lab
+if [ "%CURRENT_INSTANCE" ]
+then
+  docker rm $(docker stop $CURRENT_INSTANCE)
+fi
 
-npm install
+
+docker pull $IMAGE_NAME
+
+
+CONTAINER_EXISTS=$(docker ps -a | grep node_app)
+if [ "$CONTAINER_EXISTS" ]
+then
+  docker rm node_app
+fi
+
+
+docker create -p 8443:8443 --name node_app $IMAGE_NAME
+
 echo $privatekey > privatekey.pem
+
 echo $server > server.crt
 
-pm2 start ./bin/www --name example_app
+docker cp ./privatekey.pem node_app:/privatekey.pem
+
+docker cp ./server.crt node_app:/server.crt
+
+docker start node_app
