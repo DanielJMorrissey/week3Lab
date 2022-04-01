07962675b057fb845b089ec9d2fba6049b8f106b
DanielJMorrissey <danieljohnmorrissey@hotmail.com>
2022-04-01 13:35:23 +0100

    changing node_app and integration_app to week3Lab

--- deploy.sh
+++ deploy.sh
@@ -12,21 +12,21 @@ fi
 docker pull $IMAGE_NAME
 
 
-CONTAINER_EXISTS=$(docker ps -a | grep node_app)
+CONTAINER_EXISTS=$(docker ps -a | grep week3Lab)
 if [ "$CONTAINER_EXISTS" ]
 then
-  docker rm node_app
+  docker rm week3Lab
 fi
 
 
-docker create -p 8443:8443 --name node_app $IMAGE_NAME
+docker create -p 8443:8443 --name week3Lab $IMAGE_NAME
 
 echo $privatekey > privatekey.pem
 
 echo $server > server.crt
 
-docker cp ./privatekey.pem node_app:/privatekey.pem
+docker cp ./privatekey.pem week3Lab:/privatekey.pem
 
-docker cp ./server.crt node_app:/server.crt
+docker cp ./server.crt week3Lab:/server.crt
 
-docker start node_app
+docker start week3Lab
