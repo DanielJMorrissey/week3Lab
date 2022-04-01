9a6713e69aa29cb32258df6719f646e81a4a4271
DanielJMorrissey <danieljohnmorrissey@hotmail.com>
2022-04-01 16:18:34 +0100

    forced remove hopefully

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ CURRENT_INSTANCE=$(docker ps -a -q --filter ancestor="$IMAGE_NAME" --format="{{.
 
 if [ "$CURRENT_INSTANCE" ]
 then
-  docker rm $(docker stop $CURRENT_INSTANCE)
+  docker rm -f $(docker stop $CURRENT_INSTANCE)
 fi
 
 
@@ -15,7 +15,7 @@ docker pull $IMAGE_NAME
 CONTAINER_EXISTS=$(docker ps -a | grep week3Lab)
 if [ "$CONTAINER_EXISTS" ]
 then
-  docker rm week3Lab
+  docker rm -f week3Lab
 fi
 
 
